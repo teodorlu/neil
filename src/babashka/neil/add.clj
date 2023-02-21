e6756fcf42013f6ad4d094ed3669083b974c5d97
Teodor Heggelund <git@teod.eu>
2023-02-21 16:49:50 +0100

    New subcommand shell: `neil add bbin-installer`

---
+++ src/babashka/neil/add.clj
@@ -0,0 +1,22 @@
+(ns babashka.neil.add)
+
+;; Adding setup, docs, etc to an existing project
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Commands
+;;
+;; All public functions in babashka.neil.add are commands.
+;;
+;; A command
+;;
+;;   babashka.neil.add/bbin-installer
+;;
+;; will be invoked as
+;;
+;;   neil add bbin-installer
+;;
+;; .
+
+(defn bbin-installer
+  ""
+  [])
